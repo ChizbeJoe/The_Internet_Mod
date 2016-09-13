6cde7dce35a6226c9850d8aa45e23d305e03cfa7
ChizbeJoe <chizbejoe@gmail.com>
2016-09-13 19:13:09 -0400

    Typo fix
    Typo fix

--- Code/internetBulk.js
+++ Code/internetBulk.js
@@ -1,5 +1,5 @@
 /*
-TO-DO List (Note Done is "-". Done is "+"):
+TO-DO List (Not Done is "-". Done is "+"):
 - Make functions in mod scope
 - Make Internet Mod data save to a specfic save file (probably need to use dataStore or something)
 - Maybe have news and trends be on a website
